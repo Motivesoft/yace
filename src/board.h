6f9b67e3b2fb782fb02a3a716b0e773c9c1585ad
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2023-12-21 02:01:27 +0100

    Start to implement board

---
+++ src/board.h
@@ -0,0 +1,125 @@
+#pragma once
+
+/// <summary>
+/// Board and everything to do with it
+/// </summary>
+
+class Board
+{
+public:
+    typedef unsigned char Piece;
+    typedef unsigned long long Bitboard;
+    typedef char Index8;
+    typedef char Index64;
+    typedef unsigned int Move;
+
+    Piece empty  = 0b00000000;
+    Piece pawn   = 0b00000001;
+    Piece knight = 0b00000010;
+    Piece king   = 0b00000011;
+    Piece bishop = 0b00000110;
+    Piece rook   = 0b00000101;
+    Piece queen  = 0b00000111;
+
+    Piece slidermask = 0b00000100;
+    Piece diagonalslidermask = slidermask | 0b00000010;
+    Piece straightslidermask = slidermask | 0b00000001;
+
+    Piece colormask = 0b00001000;
+    Piece white = 0b00000000;
+    Piece black = 0b00001000;
+
+    Index64 index8Mask = 0b00000111;
+
+    inline bool isPiece( Piece piece )
+    {
+        return piece;
+    }
+
+    inline bool isNothing( Piece piece )
+    {
+        return !isPiece( piece );
+    }
+
+    inline bool isWhite( Piece piece )
+    {
+        return piece & colormask;
+    }
+
+    inline bool isBlack( Piece piece )
+    {
+        return !isWhite( piece );
+    }
+
+    inline bool isColorMatch( Piece piece1, Piece piece2 )
+    {
+        return isBlack( piece1 ) == isBlack( piece2 );
+    }
+
+    inline bool isSlider( Piece piece )
+    {
+        return ( piece & slidermask ) == slidermask;
+    }
+
+    inline bool isDiagonalSlider( Piece piece )
+    {
+        return ( piece & diagonalslidermask ) == diagonalslidermask;
+    }
+
+    inline bool isStraighSlider( Piece piece )
+    {
+        return ( piece & straightslidermask ) == straightslidermask;
+    }
+
+    inline Index64 toIndex( Index8 file, Index8 rank )
+    {
+        return ( rank << 3 ) + file;
+    }
+
+    inline Index8 toFile( Index64 index )
+    {
+        return index & index8Mask;
+    }
+
+    inline Index8 toRank( Index64 index )
+    {
+        return ( index >> 3 ) & index8Mask;
+    }
+
+    Board()
+    {
+        // Default position
+        for ( Index64 index = 32; index < 48; index++ )
+        {
+            board[ index ] = empty;
+        }
+        board[ 4 ] = white | king;
+        board[ 3 ] = white | queen;
+        board[ 2 ] = board[ 5 ] = white | bishop;
+        board[ 1 ] = board[ 6 ] = white | knight;
+        board[ 0 ] = board[ 7 ] = white | rook;
+        for ( Index64 index = 8; index < 16; index++ )
+        {
+            board[ index ] = white | pawn;
+        }
+        board[ 60 ] = black | king;
+        board[ 59 ] = black | queen;
+        board[ 58 ] = board[ 61 ] = black | bishop;
+        board[ 57 ] = board[ 62 ] = black | knight;
+        board[ 56 ] = board[ 63 ] = black | rook;
+        for ( Index64 index = 48; index < 56; index++ )
+        {
+            board[ index ] = black | pawn;
+        }
+    }
+
+    /// <summary>
+    /// No copy constructor
+    /// </summary>
+    /// <param name="board">the board to copy</param>
+    Board( const Board& board ) = delete;
+
+private:
+    Piece board[ 64 ];
+};
+
