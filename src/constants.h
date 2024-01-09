976b3d57b85d23b9f29405fbc5a21c64c6835292
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2024-01-09 12:34:50 +0100

    Bitmasks for other piece moves - bishop, rook, king, queen

--- src/constants.h
+++ src/constants.h
@@ -66,25 +66,77 @@ private:
         for ( Index64 index = 0; index < 64; index++ )
         {
             knightMoves[ index ] = 0;
+            bishopMoves[ index ] = 0;
+            rookMoves[ index ] = 0;
+            queenMoves[ index ] = 0;
+            kingMoves[ index ] = 0;
 
             Index8 file = 0b00000111 & index;
             Index8 rank = 0b00000111 & (index >> 3);
 
             signed char knightArray[][ 2 ] = { { -2, -1 }, { -2, +1 }, { +2, -1 }, { +2, +1 }, { -1, -2 }, { -1, +2 }, { +1, -2 }, { +1, +2 } };
 
-            for ( unsigned short knightArrayIndex = 0; knightArrayIndex < 8; knightArrayIndex++ )
+            for ( unsigned short arrayIndex = 0; arrayIndex < 8; arrayIndex++ )
             {
-                if ( file + knightArray[ knightArrayIndex ][ 0 ] >= 0 && file + knightArray[ knightArrayIndex ][ 0 ] < 8 )
+                if ( file + knightArray[ arrayIndex ][ 0 ] >= 0 && file + knightArray[ arrayIndex ][ 0 ] < 8 )
                 {
-                    if ( rank + knightArray[ knightArrayIndex ][ 1 ] >= 0 && rank + knightArray[ knightArrayIndex ][ 1 ] < 8 )
+                    if ( rank + knightArray[ arrayIndex ][ 1 ] >= 0 && rank + knightArray[ arrayIndex ][ 1 ] < 8 )
                     {
-                        knightMoves[ index ] |= 1ull<<(index + (( knightArray[ knightArrayIndex ][ 1 ] << 3) + knightArray[ knightArrayIndex ][ 0 ] ));
+                        knightMoves[ index ] |= 1ull<<(index + (( knightArray[ arrayIndex ][ 1 ] << 3) + knightArray[ arrayIndex ][ 0 ] ));
+                    }
+                }
+            }
+
+            signed char bishopArray[][ 2 ] = { { -1, -1 }, { -1, +1 }, { +1, -1 }, { +1, +1 } };
+
+            for ( unsigned short arrayIndex = 0; arrayIndex < 4; arrayIndex++ )
+            {
+                Index8 destinationFile = file;
+                Index8 destinationRank = rank;
+                for ( Index8 distance = 1; distance < 8; distance++ )
+                {
+                    destinationFile += bishopArray[ arrayIndex ][ 0 ];
+                    destinationRank += bishopArray[ arrayIndex ][ 1 ];
+
+                    if ( destinationFile >= 0 && destinationFile < 8 && destinationRank >= 0 && destinationRank < 8 )
+                    {
+                        bishopMoves[ index ] |= 1ull << ( ( destinationRank << 3 ) + destinationFile );
+                        queenMoves[ index ] |= 1ull << ( ( destinationRank << 3 ) + destinationFile );
+
+                        if ( distance == 1 )
+                        {
+                            kingMoves[ index ] |= 1ull << ( ( destinationRank << 3 ) + destinationFile );
+                        }
+                    }
+                }
+            }
+
+            signed char rookArray[][ 2 ] = { { -1, 0 }, { 0, -1 }, { +1, 0 }, { 0, +1 } };
+
+            for ( unsigned short arrayIndex = 0; arrayIndex < 4; arrayIndex++ )
+            {
+                Index8 destinationFile = file;
+                Index8 destinationRank = rank;
+                for ( Index8 distance = 1; distance < 8; distance++ )
+                {
+                    destinationFile += rookArray[ arrayIndex ][ 0 ];
+                    destinationRank += rookArray[ arrayIndex ][ 1 ];
+
+                    if ( destinationFile >= 0 && destinationFile < 8 && destinationRank >= 0 && destinationRank < 8 )
+                    {
+                        rookMoves[ index ] |= 1ull << ( ( destinationRank << 3 ) + destinationFile );
+                        queenMoves[ index ] |= 1ull << ( ( destinationRank << 3 ) + destinationFile );
+
+                        if ( distance == 1 )
+                        {
+                            kingMoves[ index ] |= 1ull << ( ( destinationRank << 3 ) + destinationFile );
+                        }
                     }
                 }
             }
 
             printf( "\n\n(%c%c) %d\n", 'A'+file, '1'+rank, index);
-            printBitboard( knightMoves[ index ] );
+            printBitboard( kingMoves[ index ] );
         }
     };
 };
